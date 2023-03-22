3d3b840b7982ce94e7ffef44a516e0ac48528c79
jonahscottlewis <118423801+jonahscottlewis@users.noreply.github.com>
2023-03-21 17:48:45 -0700

    added to server.js main menu and seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,24 @@
 INSERT INTO department(names)
-VALUES ()
-       ()
-       () 
+VALUES ("Engineering"),
+       ("Finance"),
+       ("Legal"),
+       ("Sales") 
 
-INSERT INTO role (title, id, department_id, salary);
-VALUES ()
-       ()
-       ()
+INSERT INTO role (id, title, department_id, salary)
+VALUES (1, "Sales Lead", "Sales", 100000),
+       (2, "Salesperson", "Sales", 80000),
+       (3, "Lead Engineer", "Engineering", 150000),
+       (4, "Software Engineer", "Engineering", 120000),
+       (5, "Accountan Manager", "Finance", 160000),
+       (6, "Accountant", "Finance", 125000),
+       (7, "Legal Team Lead", "Legal", 250000)
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id);
-VALUES ()
-       ()
-       ()
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, "John", "Doe", "Sales Lead", "null" ),
+       (2, "Mike", "Chan", "Salesperson", "John Doe" )
+       (3, "Ashley", "Rodriguez", "Lead Engineer", "null"),
+       (4, "Kevin", "Tupik", "Software Enineer", "Ashley Rodriguez"),
+       (5, "Kunal", "Singh", "Account Manager", "null"),
+       (6, "Malia", "Brown", "Accountant", "Kunal Singh"), 
+       (7, "Sarah", "Lourd", "Legal Team Lead", "null"),
+       (8, "Tom", "Allen", "Lawyer", "Sarah Lourd")
--- server.js
+++ server.js
@@ -23,21 +23,12 @@ const db = mysql.createConnection(
   console.log(`Connected to the employee_db.`)
 );
 
-connection.query(
-  'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
-  function (err, results, fields) {
-    console.log(results); // results contains rows returned by server
-    console.log(fields); // fields contains extra meta data about results, if available
-  }
-);
-
-
 const homeMenu = () => {
   return inquirer.prompt([
     {
       name: 'menu',
       type: 'list',
-      message: 'What would you like to do?',
+      message: 'Please make a selection',
       choices: [
         'View all departments',
         'View all roles',
@@ -47,14 +38,71 @@ const homeMenu = () => {
         'Add an employee',
         'Update employee role',
         // 'Update employee managers',
-        // 'View employees by manager',
+        // 'View employee by manager'
         // 'View employees by department',
         // 'Delete dapartments',
-        // 'Delete roles,',
+        // 'Delete roles',
         // 'Delete employees'
       ]
     }
   ])
+  .then((answer) => {
+    switch (answer.homeMenu) {
+      case "View all departments":
+        viewDepartments();
+        break;
+
+      case "View all roles":
+        viewRoles();
+        break;
+
+      case "View all employees":
+        viewEmployees();
+        break;  
+
+      case "Add a department":
+        addDepartment();
+        break;
+      
+      case "Add a role":
+        addRole();
+        break;
+        
+      case "Add an employee":
+        addEmployee();
+        break;
+        
+      case "Update employee role":
+        updateRole();
+        break;
+        
+      // case "Update employee managers":
+      //   updateManager();
+      //   break;
+
+      // case "View employee by manager":
+      //   viewByManager();
+      //   break;
+
+      // case "View employees by department":
+      //   viewByDepartment();
+      //   break;
+
+      // case "Delete dapartments":
+      //   deleteDepartment();
+      //   break;
+
+      // case "Delete roles":
+      //   deleteRoles();
+      //   break;
+
+      // case "Delete employees":
+      //   deleteEmployee();
+      //   break;
+
+
+    }
+  })
 }
 
 const viewDepartments = () => {
@@ -83,6 +131,52 @@ const viewEmployees = () => {
     homeMenu();
   });
 };
+
+const addDepartment = () => {
+  db.query(`UPDATE`)
+}
+
+const addRole = () => {
+  db.query(`UPDATE`)
+}
+
+const addEmployee = () => {
+  db.query(`UPDATE `)
+}
+
+const updateRole = () => {
+  db.query(`UPDATE`) 
+}
+
+// const updateManager = () => {
+//   db.query(`UPDATE`) 
+// }
+
+// const viewByManager = () => {
+//   db.query(`UPDATE`) 
+// }
+
+// const viewByDepartment = () => {
+//   db.query(`UPDATE`)
+// }
+
+
+// const deleteDepartment = () => {
+//   db.query(`UPDATE`) 
+// }
+
+// const deleteRoles = () => {
+//   db.query(`UPDATE`)
+// }
+
+// const deleteEmployee = () => {
+//   db.query(`UPDATE`)
+// }
+
+
+
+
+
 
 // Delete a movie
 app.delete('/api/movie/:id', (req, res) => {
